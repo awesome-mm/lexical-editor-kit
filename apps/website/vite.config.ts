0b0031c662f19c2c4a7ac58c3dc9a5e7e910a90b
chan <opokonse123@naver.com>
2026-02-25 21:43:42 +0900

    fix : project별 lib 중복설치 및 종속설치 에러 수정

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -3,27 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: [
-      "react",
-      "react-dom",
-      "lexical",
-      "@lexical/react",
-      "@lexical/rich-text",
-      "@lexical/list",
-      "@lexical/link",
-      "@lexical/code",
-      "@lexical/table",
-      "@lexical/code-shiki",
-      "@lexical/extension",
-      "@lexical/file",
-      "@lexical/hashtag",
-      "@lexical/markdown",
-      "@lexical/selection",
-      "@lexical/utils",
-      "@lexical/yjs",
-    ],
-  },
   server: {
     port: 5173,
   },
