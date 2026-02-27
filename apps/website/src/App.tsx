dc551d0b97b7abbe061755583c78c7f20c1b565b
chan <opokonse123@naver.com>
2026-02-27 12:44:25 +0900

    style: App 컴포넌트 패딩 및 제목 수정
    - App 컴포넌트의 패딩을 20에서 32로 변경하여 레이아웃을 개선함
    - 제목에서 "완성된 editor를 install하고 사용할거에요." 문구를 제거하여 간결하게 수정함

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -3,8 +3,8 @@ import "lexical-editor-kit/style.css";
 
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Lexical Editor Playground - 완성된 editor를 install하고 사용할거에요.</h1>
+    <div style={{ padding: 32 }}>
+      <h1>Lexical Editor Playground </h1>
       <Editor />
     </div>
   );
